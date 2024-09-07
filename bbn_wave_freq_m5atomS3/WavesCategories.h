85cdb56f4a3c7784f1527fcd308848adf66d3e21
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2024-09-06 20:33:11 -0400

    Update WavesCategories.h

--- bbn_wave_freq_m5atomS3/WavesCategories.h
+++ bbn_wave_freq_m5atomS3/WavesCategories.h
@@ -2,10 +2,12 @@
 #define WavesCategories_h
 
 /*
+  See: https://en.wikipedia.org/wiki/Douglas_sea_scale
+  
   WMO Sea State codes
-  Height (in meters):
-  Calm (glassy)   0    -  0.05  No waves breaking on beach                                                  0
-  Calm (rippled)  0.05 -  0.1   No waves breaking on beach                                                  1
+  Height (in meters_):
+  Calm (glassy)   0    -  0.10  No waves breaking on beach                                                  0
+  Calm (rippled)  0.10 -  0.1   No waves breaking on beach                                                  1
   Smooth          0.1  -  0.5   Slight waves breaking on beach                                              2
   Slight          0.5  -  1.25  Waves rock buoys and small craft                                            3
   Moderate        1.25 -  2.5   Sea becoming furrowed                                                       4
@@ -19,7 +21,7 @@
 typedef enum {
   NA = -1,
   GLASSY_SEA = 0,
-  CALM_SEA = 1,
+  CALM_RIPPLED_SEA = 1,
   SMOOTH_SEA = 2,
   SLIGHT_WAVES = 3,
   MODERATE_WAVES = 4,
