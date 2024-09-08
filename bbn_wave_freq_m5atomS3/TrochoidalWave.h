3be3117d61bfabd74097cecf23116ee8f8b59ecb
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2024-09-08 10:50:41 -0400

    Update TrochoidalWave.h

--- bbn_wave_freq_m5atomS3/TrochoidalWave.h
+++ bbn_wave_freq_m5atomS3/TrochoidalWave.h
@@ -8,6 +8,7 @@
  */
 
 float trochoid_wave_length(float periodSec);
+float trochoid_wave_period(float wave_height, float amp_range);
 
 const float g_std = 9.80665; // standard gravity acceleration m/s2
 
@@ -15,5 +16,11 @@ float trochoid_wave_length(float periodSec) {
   float lengthMeters = g_std * periodSec * periodSec / (2 * PI);
   return lengthMeters;
 }
+
+float trochoid_wave_period(float wave_height, float amp_range) {
+  float wave_period = 2.0 * PI * sqrt(fabs(wave_height / amp_range));
+  return wave_period;
+}
+
 
 #endif
