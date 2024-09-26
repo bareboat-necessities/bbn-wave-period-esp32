e0fed59675121930b0621305fa46dffc21423ef4
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2024-09-26 12:10:44 -0400

    Create WaveFilters.h

---
+++ bbn_wave_freq_m5atomS3/WaveFilters.h
@@ -0,0 +1,61 @@
+#ifndef WaveFilters_h
+#define WaveFilters_h
+
+#define FREQ_LOWER 0.004
+#define FREQ_UPPER 4.0
+#define FREQ_GUESS 0.3 /* freq guess */
+
+void init_aranovskiy(AranovskiyParams* ar_param, AranovskiyState* ar_state);
+void init_smoother(KalmanSmootherVars* kalman_smoother);
+void init_filters(AranovskiyState* ar_param, AranovskiyState* ar_state, KalmanSmootherVars* kalman_smoother);
+int warmup_time_sec(bool use_mahony);
+
+/*
+  From experiments QMEKF somehow introduces bigger bias of vertical acceleration.
+  Longer warm up time is needed to engage Aranovskiy filter.
+*/
+int warmup_time_sec(bool use_mahony) {
+  return use_mahony ? 20 : 120;
+}
+
+uint32_t getWindowMicros(double period) {
+  uint32_t windowMicros = 3 * period * 1000000;
+  if (windowMicros <= 10 * 1000000) {
+    windowMicros = 10 * 1000000;
+  }
+  else if (windowMicros >= 60 * 1000000) {
+    windowMicros = 60 * 1000000;
+  }
+  return windowMicros;
+}
+
+void init_aranovskiy(AranovskiyParams* ar_param, AranovskiyState* ar_state) {
+  /*
+    Accelerometer bias creates heave bias and Aranovskiy filter gives
+    lower frequency (i. e. higher period).
+    Even 2cm bias in heave is too much to affect frequency a lot
+  */
+  double omega_init = 0.03 * (2 * PI);  // init frequency Hz * 2 * PI (start converging from omega_init/2)
+  double k_gain = 25.0; // Aranovskiy gain. Higher value will give faster convergence, but too high will potentially overflow decimal
+  double x1_0 = 0.0;
+  double theta_0 = - (omega_init * omega_init / 4.0);
+  double sigma_0 = theta_0;
+  aranovskiy_default_params(ar_param, omega_init, k_gain);
+  aranovskiy_init_state(ar_state, x1_0, theta_0, sigma_0);
+}
+
+void init_smoother(KalmanSmootherVars* kalman_smoother) {
+  double process_noise_covariance = 0.25;
+  double measurement_uncertainty = 2.0;
+  double estimation_uncertainty = 100.0;
+  kalman_smoother_init(kalman_smoother, process_noise_covariance, measurement_uncertainty, estimation_uncertainty);
+}
+
+void init_filters(AranovskiyParams* ar_param, AranovskiyState* ar_state, KalmanSmootherVars* kalman_smoother) {
+  init_aranovskiy(ar_param, ar_state);
+  init_smoother(kalman_smoother);
+  kalman_wave_init_defaults();
+  kalman_wave_alt_init_defaults();
+}
+
+#endif
