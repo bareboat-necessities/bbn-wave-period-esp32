1e9f6af4c4017de75dd49beaf1254ad389d9e861
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-05-05 23:00:27 -0400

    Update tests.cpp

--- bbn_wave_freq_m5atomS3/tests/tests.cpp
+++ bbn_wave_freq_m5atomS3/tests/tests.cpp
@@ -27,7 +27,7 @@ KalmanSmootherVars kalman_freq;
 KalmanWaveState waveState;
 KalmanWaveAltState waveAltState;
 KalmANF kalmANF;
-SchmittTriggerFrequencyDetector freqDetector(0.0005f); // Hysteresis
+SchmittTriggerFrequencyDetector freqDetector(0.002f); // Hysteresis
 
 FrequencyTracker useFrequencyTracker = Aranovskiy;
 
