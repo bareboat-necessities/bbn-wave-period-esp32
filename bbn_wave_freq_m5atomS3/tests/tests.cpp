aa631922a64110bdb6ce9d16ec5cd93917922ab1
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-05-05 21:36:40 -0400

    Update tests.cpp

--- bbn_wave_freq_m5atomS3/tests/tests.cpp
+++ bbn_wave_freq_m5atomS3/tests/tests.cpp
@@ -68,7 +68,7 @@ void run_filters(float a, float v, float h, float delta_t) {
       float f_kalmanANF = kalmANF_process(&kalmANF, heave, delta_t, &e);
       freq = f_kalmanANF;
     } else {
-      float f_byZeroCross = freqDetector.update(heave, delta_t_inner); 
+      float f_byZeroCross = freqDetector.update(heave, delta_t); 
       freq = f_byZeroCross;
     }
     if (kalm_smoother_first) {
