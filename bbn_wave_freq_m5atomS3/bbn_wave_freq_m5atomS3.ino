864f59abf5ae455bb667b902762cb747b0d70934
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-05-08 09:23:29 -0400

    Update bbn_wave_freq_m5atomS3.ino

--- bbn_wave_freq_m5atomS3/bbn_wave_freq_m5atomS3.ino
+++ bbn_wave_freq_m5atomS3/bbn_wave_freq_m5atomS3.ino
@@ -179,7 +179,7 @@ void read_and_processIMU_data() {
           float f_kalmanANF = kalmANF_process(&kalmANF, heave, delta_t_inner, &e);
           freq = f_kalmanANF;
         } else {
-          float f_byZeroCross = freqDetector.update(a_noisy, 1.0f, 0.1f, delta_t_inner); 
+          float f_byZeroCross = freqDetector.update(a_noisy, 1.0f /* max fractions of g */, 0.1f /* debounce time */, delta_t_inner); 
           if (f_byZeroCross < FREQ_LOWER) {
             freq = FREQ_LOWER;
           } else if (f_byZeroCross > FREQ_UPPER) {
