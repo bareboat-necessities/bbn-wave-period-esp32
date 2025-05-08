65ef5a42164b79fba71e13a653f9e85f921ea81e
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-05-08 16:58:24 -0400

    Update bbn_wave_freq_m5atomS3.igual

--- bbn_wave_freq_m5atomS3/bbn_wave_freq_m5atomS3.ino
+++ bbn_wave_freq_m5atomS3/bbn_wave_freq_m5atomS3.ino
@@ -56,7 +56,7 @@ bool kalm_w_first = true, kalm_w_alt_first = true, kalm_smoother_first = true;
 //TimeAwareBandpassFilter bpFilter((FREQ_UPPER + FREQ_LOWER) / 2.0f, FREQ_UPPER - FREQ_LOWER, 0ul);  // Create a bandpass filter for 0.02-4 Hz, Center frequency: 2.01 Hz, Bandwidth: 3.98 Hz
 FourthOrderLowPass lowPassFilter(FREQ_UPPER);
 HighPassFirstOrderFilter highPassFilter((1 / FREQ_LOWER) / 2.0f /* period in sec */);
-TimeAwareSpikeFilter spikeFilter(6, 0.3);
+TimeAwareSpikeFilter spikeFilter(6, 4.0f);
 
 // frequency tracking
 SchmittTriggerFrequencyDetector freqDetector(ZERO_CROSSINGS_HYSTERESIS /* hysteresis (fractions of signal magnitude) */, ZERO_CROSSINGS_HALF_PERIODS /* half periods to run measures on */); 
