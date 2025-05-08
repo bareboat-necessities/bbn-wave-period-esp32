70ea72836f2632ca7d7ca062e6558904f331b636
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-05-08 00:46:22 -0400

    Update bbn_wave_freq_m5atomS3.ino

--- bbn_wave_freq_m5atomS3/bbn_wave_freq_m5atomS3.ino
+++ bbn_wave_freq_m5atomS3/bbn_wave_freq_m5atomS3.ino
@@ -65,7 +65,7 @@ HighPassFirstOrderFilter highPassFilter(20.0 /* period in sec */);
 TimeAwareSpikeFilter spikeFilter(6, 0.3);
 
 // frequency tracking
-SchmittTriggerFrequencyDetector freqDetector(0.06f, 1); 
+SchmittTriggerFrequencyDetector freqDetector(0.06f, 2); 
 AranovskiyParams arParams;
 AranovskiyState arState;
 KalmANF kalmANF;
