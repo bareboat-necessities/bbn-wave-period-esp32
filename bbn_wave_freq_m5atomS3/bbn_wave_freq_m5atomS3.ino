03dc5ec31232267466b2732525f56cabda842fc5
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-05-08 01:45:44 -0400

    Update bbn_wave_freq_m5atomS3.ino

--- bbn_wave_freq_m5atomS3/bbn_wave_freq_m5atomS3.ino
+++ bbn_wave_freq_m5atomS3/bbn_wave_freq_m5atomS3.ino
@@ -61,7 +61,7 @@ bool kalm_w_first = true, kalm_w_alt_first = true, kalm_smoother_first = true;
 // Basic filters
 //TimeAwareBandpassFilter bpFilter((FREQ_UPPER + FREQ_LOWER) / 2.0f, FREQ_UPPER - FREQ_LOWER, 0ul);  // Create a bandpass filter for 0.02-4 Hz, Center frequency: 2.01 Hz, Bandwidth: 3.98 Hz
 FourthOrderLowPass lowPassFilter(FREQ_UPPER);
-HighPassFirstOrderFilter highPassFilter(1 / FREQ_LOWER /* period in sec */);
+HighPassFirstOrderFilter highPassFilter((1 / FREQ_LOWER) /* period in sec */);
 TimeAwareSpikeFilter spikeFilter(6, 0.3);
 
 // frequency tracking
