02b9b7a5b3f888668910b57bedb1e7b3d99e50f1
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-06-06 12:02:41 -0400

    Update bbn_wave_freq_m5atomS3.ino

--- bbn_wave_freq_m5atomS3/bbn_wave_freq_m5atomS3.ino
+++ bbn_wave_freq_m5atomS3/bbn_wave_freq_m5atomS3.ino
@@ -357,7 +357,9 @@ void setup(void) {
   
   Matrix6f wave_dir_Q = Eigen::Matrix<float, 6, 6>::Identity() * 1e-4f;
   Matrix2f wave_dir_R = Eigen::Matrix<float, 2, 2>::Identity() * 0.09f;
-  Matrix6f wave_dir_P0 = Eigen::Matrix<float, 6, 6>::Identity() * 1.0f;
+  Matrix6f wave_dir_P0 = Eigen::Matrix<float, 6, 6>::Identity() * 100.0f;
+  wave_dir_P0(4, 4) = 1.0f;
+  wave_dir_P0(5, 5) = 1.0f;
   wave_dir_kf.init(wave_dir_Q, wave_dir_R, wave_dir_P0);
 
   wave_dir_ekf.setProcessNoise(1e-6f, 1e-6f, 1e-4f * M_PI * M_PI, 1e-6f, 1e-6f);
