e7932c9f8754b9149a378714982361dd0652a4d2
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-06-06 08:43:48 -0400

    Update bbn_wave_freq_m5atomS3.ino

--- bbn_wave_freq_m5atomS3/bbn_wave_freq_m5atomS3.ino
+++ bbn_wave_freq_m5atomS3/bbn_wave_freq_m5atomS3.ino
@@ -43,6 +43,7 @@
 #include "FourthOrderLowPass.h"
 #include "NmeaXDR.h"
 #include "KalmanQMEKF.h"
+#include "WaveDirectionEKF.h"
 #include "WaveDirection_LTV_KF.h"
 #include "WaveFilters.h"
 #include "M5_Calibr.h"
