8907baa7e7bea483ae93a68519f2489ba9fd863e
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-06-17 23:32:32 -0400

    Update bbn_wave_freq_m5atomS3.ino

--- bbn_wave_freq_m5atomS3/bbn_wave_freq_m5atomS3.ino
+++ bbn_wave_freq_m5atomS3/bbn_wave_freq_m5atomS3.ino
@@ -396,7 +396,7 @@ void loop(void) {
   M5.update();
   repeatMe();
   long duration = micros() - start;
-  long delay_micros = 3700 - duration;
+  long delay_micros = 3900 - duration;
   if (delay_micros > 0) {
     delayMicroseconds(delay_micros);
   }
