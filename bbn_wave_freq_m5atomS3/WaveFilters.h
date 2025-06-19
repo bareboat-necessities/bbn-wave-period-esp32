8c155906b5f495603a9dd6cec9c6c8bcf575e91f
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-06-19 19:50:52 -0400

    Update WaveFilters.h

--- bbn_wave_freq_m5atomS3/WaveFilters.h
+++ bbn_wave_freq_m5atomS3/WaveFilters.h
@@ -76,7 +76,7 @@ void init_smoother(KalmanSmootherVars* kalman_smoother) {
 }
 
 void init_wave_filters() {
-  kalman_wave_init_defaults(1e+1f, 1e-4f, 1e-2f, 1e-5f);
+  wave_filter.initialize(1e+1f, 1e-4f, 1e-2f, 1e-5f);
   wave_alt_filter.initialize(1e+1f, 1e-4f, 1e-2f, 5.0f, 1e-5f);
 }
 
