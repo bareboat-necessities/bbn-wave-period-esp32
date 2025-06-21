2be7f2bfca3f9428e3f44567a7db6c1195e6f247
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-06-20 21:03:38 -0400

    Update KalmanForWaveBasic.h

--- bbn_wave_freq_m5atomS3/KalmanForWaveBasic.h
+++ bbn_wave_freq_m5atomS3/KalmanForWaveBasic.h
@@ -95,7 +95,7 @@ public:
     };
 
     KalmanForWaveBasic(float q0, float q1, float q2, float q3, 
-                       float observation_noise = 0.01f, float zero_threshold = 0.2f, float correction_gain = 0.7f)
+                       float observation_noise = 0.01f, float zero_threshold = 0.05f, float correction_gain = 0.7f)
                        : zero_accel_threshold(zero_threshold), zero_correction_gain(correction_gain) {
         initialize(q0, q1, q2, q3, observation_noise);
     }
