ce7c2f776eb535887626c2a592772cc8be7957b8
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-06-24 12:09:44 -0400

    Update KalmanForWaveBasic.h

--- bbn_wave_freq_m5atomS3/KalmanForWaveBasic.h
+++ bbn_wave_freq_m5atomS3/KalmanForWaveBasic.h
@@ -63,7 +63,7 @@
 
 #include <ArduinoEigenDense.h>
 
-#define ZERO_CROSSINGS_HYSTERESIS_KF             0.5f
+#define ZERO_CROSSINGS_HYSTERESIS_KF             0.1f
 #define ZERO_CROSSINGS_VELOCITY_THRESHOLD_KF     0.4f
 #define MIN_DIVISOR_VALUE                        1e-12f  // Minimum allowed value for division operations
 
