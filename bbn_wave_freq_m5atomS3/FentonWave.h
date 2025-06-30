ae5087230ef1ff2ba3d4cdd5efeede82eb34b565
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-06-29 21:18:05 -0400

    Update FentonWave.h

--- bbn_wave_freq_m5atomS3/FentonWave.h
+++ bbn_wave_freq_m5atomS3/FentonWave.h
@@ -162,7 +162,7 @@ private:
     std::array<Real, 10> wave_height_steps(Real H, Real D, Real lam) {
         Real Hb = 0.142f * std::tanh(2 * M_PI * D / lam) * lam;
         int num = (H > 0.75f * Hb) ? 10 : (H > 0.65f * Hb) ? 5 : 3;
-        std::array<Real, 10> steps{};
+        std::vector<Real> steps(num);
         for (int i = 0; i < num; ++i)
             steps[i] = H * (i + 1) / num;
         return steps;
