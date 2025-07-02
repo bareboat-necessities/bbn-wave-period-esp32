1ded2530adc45fb66456b41ceaf5f3751c254c91
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-07-02 10:43:09 -0400

    Update FentonWaveVectorized.h

--- bbn_wave_freq_m5atomS3/FentonWaveVectorized.h
+++ bbn_wave_freq_m5atomS3/FentonWaveVectorized.h
@@ -251,11 +251,11 @@ private:
             });
         }
         const VectorF um = VectorF::Constant(-B0) + 
-                         (B.tail(N).transpose() * (kj.asDiagonal() * C1 * cos_terms)).transpose();
-        const VectorF vm = (B.tail(N).transpose() * (kj.asDiagonal() * S1 * sin_terms)).transpose();
+                         (B.tail(N).transpose() * (kj.asDiagonal() * C1.array() * cos_terms.array())).transpose();
+        const VectorF vm = (B.tail(N).transpose() * (kj.asDiagonal() * S1.array() * sin_terms.array())).transpose();
 
         residual.head(N+1) = VectorF::Constant(-B0).cwiseProduct(eta) + 
-                           (B.tail(N).transpose() * (S1 * cos_terms)).transpose() + 
+                           (B.tail(N).transpose() * (S1.array() * cos_terms.array())).transpose() + 
                            VectorF::Constant(Q);
 
         residual.segment(N+1, N+1) = 0.5 * (um.array().square() + vm.array().square()) + 
