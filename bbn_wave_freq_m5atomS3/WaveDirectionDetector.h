da1d846944670bee509a9c7fd305b19df0ce1d41
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-07-07 15:08:22 -0400

    Update WaveDirectionDetector.h

--- bbn_wave_freq_m5atomS3/WaveDirectionDetector.h
+++ bbn_wave_freq_m5atomS3/WaveDirectionDetector.h
@@ -10,8 +10,8 @@ class WaveDirectionDetector {
 private:
   const float alpha;
   const float threshold;
-  float prevVertAccel = 0;
-  float filteredP = 0;
+  float prevVertAccel = NAN;
+  float filteredP = 0.0f;
 
 public:
   // waveAngle in radians (0=positive X, PI/2=positive Y)
@@ -24,6 +24,10 @@ public:
   // Processes X,Y,Z accelerations
   WaveDirection update(float accelX, float accelY, float accelZ, float delta_t) {
     float mag_a = sqrtf(accelX * accelX + accelY * accelY);
+    if (std::isnan(prevVertAccel)) {
+      prevVertAccel = accelZ;
+      return UNCERTAIN;
+    }
     if (mag_a > 1e-8f) {
       // Project X/Y onto wave direction axis
       float aHoriz = accelY > 0 ? mag_a : -mag_a;
