6acb57174937cdc792884b2b1d83cc7d5d43727a
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-07-14 21:09:51 -0400

    Update KalmanWaveDirection.h

--- bbn_wave_freq_m5atomS3/KalmanWaveDirection.h
+++ bbn_wave_freq_m5atomS3/KalmanWaveDirection.h
@@ -139,7 +139,7 @@ void KalmanWaveDirection_test_1() {
   filter.setMeasurementNoise(0.01f);
   filter.setProcessNoise(1e-6f);
 
-  std::ofstream out("wavedir.csv");
+  std::ofstream out("wave_dir.csv");
   out << "t,ax,ay,filtered_ax,filtered_ay,frequency,amplitude,phase,confidence,deg\n";
 
   for (int i = 0; i < num_steps; ++i) {
