57ec6c70b965151dffef2ff1b5a4b737e2159916
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-07-22 21:39:24 -0400

    Update fenton_plots.cpp

--- bbn_wave_freq_m5atomS3/plots/fenton_plots.cpp
+++ bbn_wave_freq_m5atomS3/plots/fenton_plots.cpp
@@ -1,4 +1,12 @@
+/*
+  Copyright 2024-2025, Mikhail Grushinskiy
+*/
+
+#define FENTON_TEST
+
+#include "FentonWaveVectorized.h"
 
 int main(int argc, char *argv[]) {
+  FentonWave_test_1();
+  FentonWave_test_2();
 }
-
