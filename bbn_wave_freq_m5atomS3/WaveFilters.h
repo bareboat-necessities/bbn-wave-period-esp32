9370d02392163abc95863ee1357ecd99425fad7c
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-07-24 15:12:10 -0400

    Update WaveFilters.h

--- bbn_wave_freq_m5atomS3/WaveFilters.h
+++ bbn_wave_freq_m5atomS3/WaveFilters.h
@@ -63,7 +63,7 @@ void init_aranovskiy(AranovskiyFilter<double>* ar_filter) {
     lower frequency (i. e. higher period).
     Even 2cm bias in heave is too much to affect frequency a lot
   */
-  double omega_init = (FREQ_GUESS * 2) * (2 * PI);  // init frequency Hz * 2 * PI (start converging from omega_init/2)
+  double omega_init = (FREQ_GUESS * 2) * (2 * M_PI);  // init frequency Hz * 2 * PI (start converging from omega_init/2)
   double k_gain = 8.0; // Aranovskiy gain. Higher value will give faster convergence, but too high will potentially overflow decimal
   double x1_0 = 0.0;
   double theta_0 = - (omega_init * omega_init / 4.0);
