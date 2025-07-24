b7bf04ab43f4869f8f6e6bde908b79835d2dc21e
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-07-24 12:36:07 -0400

    Create wave_dir_plots.cpp

---
+++ bbn_wave_freq_m5atomS3/plots/wave_dir_plots.cpp
@@ -0,0 +1,12 @@
+/*
+  Copyright 2024-2025, Mikhail Grushinskiy
+*/
+
+#define KALMAN_WAVE_DIRECTION_TEST
+#define EIGEN_NON_ARDUINO
+
+#include "KalmanWaveDirection.h"
+
+int main(int argc, char *argv[]) {
+  KalmanWaveDirection_test_1();
+}
