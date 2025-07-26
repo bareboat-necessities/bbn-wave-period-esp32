4515b1083ae81ea2ad7152eaa3f2bd06931ee90d
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-07-25 22:19:24 -0400

    Update tests.cpp

--- bbn_wave_freq_m5atomS3/tests/tests.cpp
+++ bbn_wave_freq_m5atomS3/tests/tests.cpp
@@ -143,6 +143,9 @@ int main(int argc, char *argv[]) {
     init_filters(&arFilter, &kalman_freq);
   } else if (useFrequencyTracker == Kalm_ANF) {
     init_filters_alt(&kalmANF, &kalman_freq);
+  } else if (useFrequencyTracker == EKF_Oscillator) {
+    ekf_oscillator.setProcessNoise(1e-4f, 1e-5f, 1e-4f);
+    ekf_oscillator.setMeasurementNoise(0.01f);
   } else {
     kalman_smoother_init(&kalman_freq, 0.25f, 2.0f, 100.0f);
     init_wave_filters();
