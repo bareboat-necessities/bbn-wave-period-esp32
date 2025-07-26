bbda7d0fe5e1745b3b5844ec3ad55ec1ec6aa60a
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-07-25 23:09:14 -0400

    Update tests.cpp

--- bbn_wave_freq_m5atomS3/tests/tests.cpp
+++ bbn_wave_freq_m5atomS3/tests/tests.cpp
@@ -38,7 +38,7 @@ SchmittTriggerFrequencyDetector freqDetector(ZERO_CROSSINGS_HYSTERESIS, ZERO_CRO
 TimeAwareSpikeFilter spikeFilter(ACCEL_SPIKE_FILTER_SIZE, ACCEL_SPIKE_FILTER_THRESHOLD);
 WaveSurfaceProfile<128> waveProfile;
 
-FrequencyTracker useFrequencyTracker = ZeroCrossing; // EKF_Oscillator;
+FrequencyTracker useFrequencyTracker = EKF_Oscillator; // ZeroCrossing;
 
 bool kalm_w_first = true, kalm_w_alt_first = true, kalm_smoother_first = true;
 
