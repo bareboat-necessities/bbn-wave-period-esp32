789f54c615427ba6ade6cddd8b894a4741c502f9
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-07-26 10:42:21 -0400

    Update EKF_HarmonicOscillator.h

--- bbn_wave_freq_m5atomS3/EKF_HarmonicOscillator.h
+++ bbn_wave_freq_m5atomS3/EKF_HarmonicOscillator.h
@@ -40,13 +40,13 @@ public:
         
         // Process noise
         Q.setIdentity(); 
-        Q *= Real(1e-3);
-        Q(2 * M, 2 * M) = Real(1e-2);      // Frequency process noise
-        Q(2 * M + 1, 2 * M + 1) = Real(1e-4); // Bias process noise
+        Q *= Real(1e-4);
+        Q(2 * M, 2 * M) = Real(1e-1);      // Frequency process noise
+        Q(2 * M + 1, 2 * M + 1) = Real(1e-5); // Bias process noise
         
         // Measurement noise
         R.setZero();
-        R(0, 0) = Real(0.01);
+        R(0, 0) = Real(0.1);
         
         // Calculate weights
         calculateWeights();
