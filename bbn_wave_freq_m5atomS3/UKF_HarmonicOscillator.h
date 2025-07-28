7452ef21425e44207a3f7731947f46ac4f385229
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-07-28 19:51:51 -0400

    Update UKF_HarmonicOscillator.h

--- bbn_wave_freq_m5atomS3/UKF_HarmonicOscillator.h
+++ bbn_wave_freq_m5atomS3/UKF_HarmonicOscillator.h
@@ -37,9 +37,9 @@ public:
         P(2 * M + 2, 2 * M + 2) = Real(M_PI * M_PI / 4); // phase
         
         Q.setIdentity(); Q *= Real(1e-3);
-        Q(2 * M, 2 * M) = Real(1e-4);         // omega process noise
+        Q(2 * M, 2 * M) = Real(1e-5);         // omega process noise
         Q(2 * M + 1, 2 * M + 1) = Real(1e-7); // bias
-        Q(2 * M + 2, 2 * M + 2) = Real(1e-6); // phase
+        Q(2 * M + 2, 2 * M + 2) = Real(1e-7); // phase
 
         R.setZero(); R(0, 0) = Real(0.5);
 
