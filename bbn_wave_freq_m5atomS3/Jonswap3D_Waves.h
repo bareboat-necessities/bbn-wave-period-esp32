ec148fbb2ff5f45ea4edbaa131970ac2c37092e0
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-07-30 13:25:56 -0400

    Update Jonswap3D_Waves.h

--- bbn_wave_freq_m5atomS3/Jonswap3D_Waves.h
+++ bbn_wave_freq_m5atomS3/Jonswap3D_Waves.h
@@ -68,16 +68,16 @@ public:
         };
     }
 
-Eigen::Matrix<double, N_FREQ, 3> exportSpectrum() const {
-    Eigen::Matrix<double, N_FREQ, 3> result;
-    for (int i = 0; i < N_FREQ; ++i) {
-        double dir_angle = std::atan2(dir_y_(i), dir_x_(i)); // from direction vector
-        result(i, 0) = frequencies_(i); // Hz
-        result(i, 1) = A_(i);           // amplitude
-        result(i, 2) = dir_angle;       // radians
+    Eigen::Matrix<double, N_FREQ, 3> exportSpectrum() const {
+        Eigen::Matrix<double, N_FREQ, 3> result;
+        for (int i = 0; i < N_FREQ; ++i) {
+            double dir_angle = std::atan2(dir_y_(i), dir_x_(i)); 
+            result(i, 0) = frequencies_(i);
+            result(i, 1) = A_(i);          
+            result(i, 2) = dir_angle;     
+        }
+        return result;
     }
-    return result;
-}
 
 private:
     double Hs_, Tp_, gamma_, g_;
