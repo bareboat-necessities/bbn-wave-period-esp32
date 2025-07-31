e79015cc440d39c9fb4cc7ad89c1a23f333027a0
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-07-31 17:53:43 -0400

    Update WaveFilters.h

--- bbn_wave_freq_m5atomS3/WaveFilters.h
+++ bbn_wave_freq_m5atomS3/WaveFilters.h
@@ -66,7 +66,7 @@ void init_aranovskiy(AranovskiyFilter<double>* ar_filter) {
   double omega_init = (FREQ_GUESS * 2) * (2 * M_PI);  // init frequency Hz * 2 * PI (start converging from omega_init/2)
   double k_gain = 8.0; // Aranovskiy gain. Higher value will give faster convergence, but too high will potentially overflow decimal
   double x1_0 = 0.0;
-  double theta_0 = - (omega_init * omega_init / 4.0);
+  double theta_0 = -0.09; // -(omega_init * omega_init / 4.0);
   double sigma_0 = theta_0;
   ar_filter->setParams(omega_init, k_gain);
   ar_filter->setState(x1_0, theta_0, sigma_0);
