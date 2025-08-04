a7960621b80af9ef1703c495749c8a438ecb2b33
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-08-04 13:04:11 -0400

    Update AranovskiyFilter.h

--- bbn_wave_freq_m5atomS3/AranovskiyFilter.h
+++ bbn_wave_freq_m5atomS3/AranovskiyFilter.h
@@ -88,7 +88,7 @@ public:
 
       // Update theta and omega
       theta = sigma + k * b * x1 * y;
-      omega = std::sqrt(std::max(Real(1e-12), std::abs(theta)));
+      omega = std::sqrt(std::max(Real(1e-7), std::abs(theta)));
       f = omega / (Real(2) * M_PI);
 
       // State integration
