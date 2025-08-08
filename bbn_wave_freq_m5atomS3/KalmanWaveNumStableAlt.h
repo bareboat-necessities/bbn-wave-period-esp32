7063be0e8a5e65b3c2d810113ab5ea206c7f043f
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-08-08 16:42:56 -0400

    Update KalmanWaveNumStableAlt.h

--- bbn_wave_freq_m5atomS3/KalmanWaveNumStableAlt.h
+++ bbn_wave_freq_m5atomS3/KalmanWaveNumStableAlt.h
@@ -113,10 +113,10 @@ public:
     };
 
     KalmanWaveNumStableAlt(
-            float q0_at_f_low = 2000.0f, float q0_at_f_high = 2.0f,
-            float q1_at_f_low = 5e-2f, float q1_at_f_high = 1e-4f,
+            float q0_at_f_low = 8000.0f, float q0_at_f_high = 2.0f,
+            float q1_at_f_low = 10e-2f, float q1_at_f_high = 1e-4f,
             float q2_at_f_low = 4e-2f, float q2_at_f_high = 1e-2f,
-            float q3 = 1e+6f, float q4 = 1e-5f,
+            float q3 = 4e+6f, float q4 = 1e-5f,
             float temperature_drift_coeff = 0.007f)
     {
         initialize(q0_at_f_low, q0_at_f_high, q1_at_f_low, q1_at_f_high,
