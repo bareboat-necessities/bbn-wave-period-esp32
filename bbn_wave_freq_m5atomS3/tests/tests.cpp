63e7bbf67f53beabab47c55a52ce130685b9d3c5
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-08-07 22:01:09 -0400

    Update tests.cpp

--- bbn_wave_freq_m5atomS3/tests/tests.cpp
+++ bbn_wave_freq_m5atomS3/tests/tests.cpp
@@ -144,7 +144,7 @@ int main(int argc, char *argv[]) {
 
   float sample_freq = 250.0; // Hz
   float delta_t = 1.0 / sample_freq;
-  float test_duration = 10.0 * 60.0;
+  float test_duration = 3.0 * 60.0;
 
   if (useFrequencyTracker == Aranovskiy) {
     init_filters(&arFilter, &kalman_freq);
