fb0418a25ad754e3bd2e6c84ea20ce8f6b93227b
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-08-12 14:20:21 -0400

    Update WaveFilters.h

--- bbn_wave_freq_m5atomS3/WaveFilters.h
+++ bbn_wave_freq_m5atomS3/WaveFilters.h
@@ -9,11 +9,11 @@
 #define FREQ_UPPER 2.0f
 #define FREQ_GUESS 0.3f   // frequency guess
 
-#define ZERO_CROSSINGS_HYSTERESIS     0.05f
+#define ZERO_CROSSINGS_HYSTERESIS     0.04f
 #define ZERO_CROSSINGS_PERIODS        1
 #define ZERO_CROSSINGS_SCALE          1.0f
 #define ZERO_CROSSINGS_DEBOUNCE_TIME  0.12f
-#define ZERO_CROSSINGS_STEEPNESS_TIME 0.16f
+#define ZERO_CROSSINGS_STEEPNESS_TIME 0.22f
 
 #define FREQ_COEF        3.0f
 
