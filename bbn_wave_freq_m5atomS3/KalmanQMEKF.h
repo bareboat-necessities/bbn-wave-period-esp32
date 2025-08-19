e233d136beaa43821c2b4695e067e21e72a230c5
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-08-19 18:56:38 -0400

    Update KalmanQMEKF.h

--- bbn_wave_freq_m5atomS3/KalmanQMEKF.h
+++ bbn_wave_freq_m5atomS3/KalmanQMEKF.h
@@ -48,8 +48,8 @@ class QuaternionMEKF {
     static constexpr T half = T(1) / T(2);
 
   public:
-    QuaternionMEKF(Vector3 const& sigma_a, Vector3 const& sigma_g, Vector3 const& sigma_m, T Pq0, T Pb0, T b0);
-    constexpr QuaternionMEKF(T const sigma_a[3], T const sigma_g[3], T const sigma_m[3], T Pq0, T Pb0, T b0);
+    QuaternionMEKF(Vector3 const& sigma_a, Vector3 const& sigma_g, Vector3 const& sigma_m, T Pq0 = T(1e-6), T Pb0, T b0);
+    constexpr QuaternionMEKF(T const sigma_a[3], T const sigma_g[3], T const sigma_m[3], T Pq0 = T(1e-6), T Pb0, T b0);
     void initialize_from_acc_mag(Vector3 const& acc, Vector3 const& mag);
     void initialize_from_acc_mag(T const acc[3], T const mag[3]);
     void initialize_from_acc(Vector3 const& acc);
@@ -96,7 +96,7 @@ class QuaternionMEKF {
 };
 
 template <typename T, bool with_bias>
-QuaternionMEKF<T, with_bias>::QuaternionMEKF(Vector3 const& sigma_a, Vector3 const& sigma_g, Vector3 const& sigma_m, T Pq0 = T(1e-6), T Pb0 = T(1e-1), T b0 = T(1e-12))
+QuaternionMEKF<T, with_bias>::QuaternionMEKF(Vector3 const& sigma_a, Vector3 const& sigma_g, Vector3 const& sigma_m, T Pq0, T Pb0 = T(1e-1), T b0 = T(1e-12))
   : Q(initialize_Q(sigma_g, b0)),
   Racc(sigma_a.array().square().matrix().asDiagonal()),
   Rmag(sigma_m.array().square().matrix().asDiagonal()),
