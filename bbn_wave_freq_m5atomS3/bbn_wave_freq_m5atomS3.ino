95af22c43a9b235a351e7c0c63919f0bb66108ae
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-08-21 14:25:02 -0400

    Update bbn_wave_freq_m5atomS3.ino

--- bbn_wave_freq_m5atomS3/bbn_wave_freq_m5atomS3.ino
+++ bbn_wave_freq_m5atomS3/bbn_wave_freq_m5atomS3.ino
@@ -45,6 +45,7 @@
 #include "WaveFilters.h"
 #include "WaveDirectionDetector.h"
 #include "M5_Calibr.h"
+#include "Kalman3D-unfinished.h"
 
 bool useMahony = true;
 FrequencyTracker useFrequencyTracker = Aranovskiy;
