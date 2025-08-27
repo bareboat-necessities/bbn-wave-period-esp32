791fbefc4ef191b627853aba9124d6bddcc5ef19
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-08-27 12:20:18 -0400

    Update Jonswap3dStokesWaves.h

--- bbn_wave_freq_m5atomS3/Jonswap3dStokesWaves.h
+++ bbn_wave_freq_m5atomS3/Jonswap3dStokesWaves.h
@@ -122,19 +122,6 @@ private:
 };
 
 // Jonswap3dStokesWaves
-#pragma once
-
-#ifdef EIGEN_NON_ARDUINO
-#include <Eigen/Dense>
-#else
-#include <ArduinoEigenDense.h>
-#endif
-#include <random>
-#include <cmath>
-#include <stdexcept>
-#include <string>
-#include <algorithm>
-
 template<int N_FREQ = 256>
 class Jonswap3dStokesWaves {
 public:
