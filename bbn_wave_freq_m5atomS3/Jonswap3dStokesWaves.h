240e3c767a767398e4f96cf72c847927afe5b534
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-08-27 19:01:45 -0400

    Update Jonswap3dStokesWaves.h

--- bbn_wave_freq_m5atomS3/Jonswap3dStokesWaves.h
+++ bbn_wave_freq_m5atomS3/Jonswap3dStokesWaves.h
@@ -133,18 +133,18 @@ public:
         Eigen::Vector3d acceleration;
     };
 
-    // Constructor
     Jonswap3dStokesWaves(double Hs, double Tp,
                          double mean_direction_deg = 0.0,
                          double f_min = 0.02, double f_max = 0.8,
                          double gamma = 2.0, double g = 9.81,
                          double spreading_exponent = 15.0,
-                         unsigned int seed = 239u)
-        : Hs_(Hs), Tp_(Tp), mean_dir_rad_(mean_direction_deg * M_PI / 180.0),
+                         unsigned int seed = 239u,
+                         bool useSecondOrderApprox = true,
+                         int N_bands = 32)
+        : Hs_(Hs), Tp_(Tp), mean_dir_rad_(mean_direction_deg*M_PI/180.0),
           gamma_(gamma), g_(g), spreading_exponent_(spreading_exponent),
-          seed_(seed), pairwise_size_((size_t)N_FREQ*(N_FREQ+1)/2),
-          spectrum_(Hs, Tp, f_min, f_max, gamma, g),
-          exp_kz_cached_z_(std::numeric_limits<double>::quiet_NaN())
+          seed_(seed), useSecondOrderApprox_(useSecondOrderApprox), N_bands_(N_bands),
+          spectrum_(Hs, Tp, f_min, f_max, gamma, g)
     {
         frequencies_ = spectrum_.frequencies();
         S_ = spectrum_.spectrum();
@@ -154,69 +154,99 @@ public:
         omega_ = 2.0 * M_PI * frequencies_;
         k_ = omega_.array().square() / g_;
 
-        dir_x_.setZero();
-        dir_y_.setZero();
-        kx_.setZero();
-        ky_.setZero();
-        phi_.setZero();
-
         initializeRandomPhases();
         initializeDirectionalSpread();
         computeWaveDirectionComponents();
         computePerComponentStokesDriftEstimate();
-        precomputePairwise();
+
+        if(!useSecondOrderApprox_)
+            precomputePairwise();
+        else
+            precomputeBandedSecondOrder(N_bands_);
+
         checkSteepness();
     }
 
-    // Get Lagrangian state at single point (x,y) and time t
     WaveState getLagrangianState(double x, double y, double t, double z=0.0) const
     {
-        return evalLagrangianCached(
-            x, y, t,
-            // First-order lambda
-            [&](Eigen::Vector3d &disp, Eigen::Vector3d &vel, Eigen::Vector3d &acc,
-                int i, double sin_th, double cos_th)
-            {
-                disp.x() += -A_(i) * cos_th * dir_x_(i);
-                disp.y() += -A_(i) * cos_th * dir_y_(i);
-                disp.z() +=  A_(i) * sin_th;
+        Eigen::Vector3d disp=Eigen::Vector3d::Zero();
+        Eigen::Vector3d vel=Eigen::Vector3d::Zero();
+        Eigen::Vector3d acc=Eigen::Vector3d::Zero();
 
-                double fac = A_(i) * omega_(i);
-                vel.x() += fac * sin_th * dir_x_(i);
-                vel.y() += fac * sin_th * dir_y_(i);
-                vel.z() += fac * cos_th;
+        // First-order
+        Eigen::ArrayXd theta0 = kx_.array()*x + ky_.array()*y + phi_.array();
+        Eigen::ArrayXd sin_first = (theta0 - omega_*t).sin();
+        Eigen::ArrayXd cos_first = (theta0 - omega_*t).cos();
 
-                fac = A_(i) * omega_(i) * omega_(i);
-                acc.x() += fac * cos_th * dir_x_(i);
-                acc.y() += fac * cos_th * dir_y_(i);
-                acc.z() += -fac * sin_th;
-            },
-            // Second-order lambda
-            [&](Eigen::Vector3d &disp, Eigen::Vector3d &vel, Eigen::Vector3d &acc,
-                size_t idx, double factor, double sin_th, double cos_th)
-            {
-                disp.z() += factor * Bij_flat_[idx] * cos_th;
-                double ksum2 = k_sum_flat_[idx]*k_sum_flat_[idx];
-                if(ksum2>1e-18){
-                    double hx = kx_sum_flat_[idx]/k_sum_flat_[idx];
-                    double hy = ky_sum_flat_[idx]/k_sum_flat_[idx];
-                    disp.x() += factor * (-Bij_flat_[idx]) * cos_th * hx;
-                    disp.y() += factor * (-Bij_flat_[idx]) * cos_th * hy;
+        for(int i=0;i<N_FREQ;++i){
+            disp.x() += -A_(i)*cos_first(i)*dir_x_(i);
+            disp.y() += -A_(i)*cos_first(i)*dir_y_(i);
+            disp.z() +=  A_(i)*sin_first(i);
+
+            double fac = A_(i)*omega_(i);
+            vel.x() += fac*sin_first(i)*dir_x_(i);
+            vel.y() += fac*sin_first(i)*dir_y_(i);
+            vel.z() += fac*cos_first(i);
+
+            fac = A_(i)*omega_(i)*omega_(i);
+            acc.x() += fac*cos_first(i)*dir_x_(i);
+            acc.y() += fac*cos_first(i)*dir_y_(i);
+            acc.z() += -fac*sin_first(i);
+        }
+
+        // Second-order
+        if(useSecondOrderApprox_){
+            for(int i=0;i<N_bands_;++i)
+                for(int j=i;j<N_bands_;++j){
+                    double factor = (i==j)?1.0:2.0;
+                    double Bij = (band_kx_[i]*band_kx_[j]+band_ky_[i]*band_ky_[j])/(2.0*g_)*band_A_[i]*band_A_[j];
+                    double sumOmega = band_omega_[i]+band_omega_[j];
+                    double theta = (band_kx_[i]+band_kx_[j])*x + (band_ky_[i]+band_ky_[j])*y + band_phi_[i]+band_phi_[j]-sumOmega*t;
+                    double sinTh = std::sin(theta);
+                    double cosTh = std::cos(theta);
+
+                    disp.z() += factor * Bij * cosTh;
+                    double ksum2 = (band_kx_[i]+band_kx_[j])*(band_kx_[i]+band_kx_[j])+(band_ky_[i]+band_ky_[j])*(band_ky_[i]+band_ky_[j]);
+                    if(ksum2>1e-18){
+                        double hx=(band_kx_[i]+band_kx_[j])/std::sqrt(ksum2);
+                        double hy=(band_ky_[i]+band_ky_[j])/std::sqrt(ksum2);
+                        disp.x() += factor*(-Bij)*cosTh*hx;
+                        disp.y() += factor*(-Bij)*cosTh*hy;
+
+                        vel.x() += factor*(-Bij)*sumOmega*sinTh*hx;
+                        vel.y() += factor*(-Bij)*sumOmega*sinTh*hy;
+                    }
+                    vel.z() += factor*(-Bij)*sumOmega*sinTh;
                 }
+        } else {
+            // Full pairwise calculation (same as original)
+            for(int i=0;i<N_FREQ;++i)
+                for(int j=i;j<N_FREQ;++j){
+                    size_t idx = upper_index(i,j);
+                    double factor = (i==j)?1.0:2.0;
+                    double ksum2 = k_sum_flat_[idx]*k_sum_flat_[idx];
+                    double hx = (ksum2>1e-18)? kx_sum_flat_[idx]/k_sum_flat_[idx]:0.0;
+                    double hy = (ksum2>1e-18)? ky_sum_flat_[idx]/k_sum_flat_[idx]:0.0;
 
-                double sum_omega = omega_sum_flat_[idx];
-                double hx = (ksum2>1e-18)? kx_sum_flat_[idx]/k_sum_flat_[idx]:0.0;
-                double hy = (ksum2>1e-18)? ky_sum_flat_[idx]/k_sum_flat_[idx]:0.0;
-                vel.x() += factor * (-Bij_flat_[idx]) * sum_omega * sin_th * hx;
-                vel.y() += factor * (-Bij_flat_[idx]) * sum_omega * sin_th * hy;
-                vel.z() += factor * (-Bij_flat_[idx]) * sum_omega * sin_th;
+                    double theta = kx_sum_flat_[idx]*x+ky_sum_flat_[idx]*y+phi_sum_flat_[idx]-omega_sum_flat_[idx]*t;
+                    double sinTh = std::sin(theta);
+                    double cosTh = std::cos(theta);
 
-                double sum_omega2 = sum_omega*sum_omega;
-                acc.x() += factor * Bij_flat_[idx] * sum_omega2 * cos_th * hx;
-                acc.y() += factor * Bij_flat_[idx] * sum_omega2 * cos_th * hy;
-                acc.z() += factor * Bij_flat_[idx] * sum_omega2 * cos_th;
-            }
-        );
+                    disp.z() += factor * Bij_flat_[idx]*cosTh;
+                    disp.x() += factor*(-Bij_flat_[idx])*cosTh*hx;
+                    disp.y() += factor*(-Bij_flat_[idx])*cosTh*hy;
+
+                    vel.x() += factor*(-Bij_flat_[idx])*omega_sum_flat_[idx]*sinTh*hx;
+                    vel.y() += factor*(-Bij_flat_[idx])*omega_sum_flat_[idx]*sinTh*hy;
+                    vel.z() += factor*(-Bij_flat_[idx])*omega_sum_flat_[idx]*sinTh;
+                }
+        }
+
+        // Add mean Stokes drift
+        vel.x() += stokes_drift_mean_xy_.x();
+        vel.y() += stokes_drift_mean_xy_.y();
+
+        return {disp, vel, acc};
     }
 
 private:
@@ -224,44 +254,39 @@ private:
     JonswapSpectrum<N_FREQ> spectrum_;
     double Hs_, Tp_, mean_dir_rad_, gamma_, g_, spreading_exponent_;
     unsigned int seed_;
-    size_t pairwise_size_;
+    bool useSecondOrderApprox_;
+    int N_bands_;
 
     Eigen::Matrix<double, N_FREQ, 1> frequencies_, S_, A_, df_;
     Eigen::Matrix<double, N_FREQ, 1> omega_, k_, phi_;
     Eigen::Matrix<double, N_FREQ, 1> dir_x_, dir_y_, kx_, ky_;
-    Eigen::Matrix<double, N_FREQ, 1> stokes_drift_scalar_;
     Eigen::Vector2d stokes_drift_mean_xy_;
 
+    // Full pairwise arrays
     std::vector<double> Bij_flat_, kx_sum_flat_, ky_sum_flat_, k_sum_flat_, omega_sum_flat_, phi_sum_flat_;
-    mutable std::vector<double> exp_kz_cache_;
-    mutable double exp_kz_cached_z_;
+    size_t pairwise_size_;
 
-    // Trig caching per time
-    struct TrigCache {
-        Eigen::ArrayXd sin_first, cos_first;
-        Eigen::ArrayXd sin_second, cos_second;
-        double last_t = std::numeric_limits<double>::quiet_NaN();
-    };
-    mutable TrigCache trig_cache_;
+    // Banded arrays
+    std::vector<double> band_A_, band_kx_, band_ky_, band_omega_, band_phi_;
 
-    // --- Initialization helpers ---
+    // Initialization helpers
     void initializeRandomPhases() {
         std::mt19937 gen(seed_);
         std::uniform_real_distribution<double> dist(0.0, 2.0*M_PI);
-        for(int i=0;i<N_FREQ;++i) phi_(i) = dist(gen);
+        for(int i=0;i<N_FREQ;++i) phi_(i)=dist(gen);
     }
 
     void initializeDirectionalSpread() {
         std::mt19937 gen(seed_+1);
-        std::uniform_real_distribution<double> u_dist(0.0, 2.0*M_PI);
-        std::uniform_real_distribution<double> y_dist(0.0, 1.0);
+        std::uniform_real_distribution<double> u_dist(0.0,2.0*M_PI);
+        std::uniform_real_distribution<double> y_dist(0.0,1.0);
         for(int i=0;i<N_FREQ;++i){
             double theta=0.0;
             while(true){
-                double candidate=u_dist(gen);
-                double clamped=std::max(0.0,std::cos(candidate-mean_dir_rad_));
-                double pdf_val=std::pow(clamped,spreading_exponent_);
-                if(y_dist(gen)<=pdf_val){theta=candidate; break;}
+                double candidate = u_dist(gen);
+                double clamped = std::max(0.0,std::cos(candidate-mean_dir_rad_));
+                double pdf_val = std::pow(clamped,spreading_exponent_);
+                if(y_dist(gen)<=pdf_val){ theta=candidate; break; }
             }
             dir_x_(i)=std::cos(theta);
             dir_y_(i)=std::sin(theta);
@@ -270,17 +295,15 @@ private:
 
     void computeWaveDirectionComponents() {
         for(int i=0;i<N_FREQ;++i){
-            kx_(i) = k_(i) * dir_x_(i);
-            ky_(i) = k_(i) * dir_y_(i);
+            kx_(i) = k_(i)*dir_x_(i);
+            ky_(i) = k_(i)*dir_y_(i);
         }
     }
 
     void computePerComponentStokesDriftEstimate() {
-        stokes_drift_scalar_.setZero();
         stokes_drift_mean_xy_.setZero();
         for(int i=0;i<N_FREQ;++i){
             double Usi=0.5*A_(i)*A_(i)*k_(i)*omega_(i);
-            stokes_drift_scalar_(i)=Usi;
             stokes_drift_mean_xy_.x() += Usi*dir_x_(i);
             stokes_drift_mean_xy_.y() += Usi*dir_y_(i);
         }
@@ -297,13 +320,13 @@ private:
 
         for(int i=0;i<N_FREQ;++i){
             for(int j=i;j<N_FREQ;++j){
-                size_t idx = upper_index(i,j);
-                double kx_sum = kx_(i)+kx_(j);
-                double ky_sum = ky_(i)+ky_(j);
+                size_t idx=upper_index(i,j);
+                double kx_sum=kx_(i)+kx_(j);
+                double ky_sum=ky_(i)+ky_(j);
                 double kdot = kx_(i)*kx_(j)+ky_(i)*ky_(j);
-                double Bij = (kdot)/(2.0*g_)*(A_(i)*A_(j));
-                double omega_sum = omega_(i)+omega_(j);
-                double phi_sum = phi_(i)+phi_(j);
+                double Bij = kdot/(2.0*g_)*(A_(i)*A_(j));
+                double omega_sum=omega_(i)+omega_(j);
+                double phi_sum=phi_(i)+phi_(j);
                 double ksum = std::sqrt(kx_sum*kx_sum + ky_sum*ky_sum);
 
                 Bij_flat_[idx]=Bij;
@@ -314,8 +337,34 @@ private:
                 phi_sum_flat_[idx]=phi_sum;
             }
         }
-        exp_kz_cache_.assign(pairwise_size_, std::numeric_limits<double>::quiet_NaN());
-        exp_kz_cached_z_ = std::numeric_limits<double>::quiet_NaN();
+    }
+
+    void precomputeBandedSecondOrder(int N_bands) {
+        band_A_.resize(N_bands);
+        band_kx_.resize(N_bands);
+        band_ky_.resize(N_bands);
+        band_omega_.resize(N_bands);
+        band_phi_.resize(N_bands);
+
+        int band_size = N_FREQ / N_bands;
+        for(int b=0;b<N_bands;++b){
+            int start = b*band_size;
+            int end = std::min(start+band_size,N_FREQ);
+            double sumA=0.0, sumkx=0.0, sumky=0.0, sumw=0.0, sumphi=0.0;
+            for(int i=start;i<end;++i){
+                sumA += A_(i);
+                sumkx += kx_(i);
+                sumky += ky_(i);
+                sumw += omega_(i);
+                sumphi += phi_(i);
+            }
+            int count=end-start;
+            band_A_[b] = sumA/count;
+            band_kx_[b] = sumkx/count;
+            band_ky_[b] = sumky/count;
+            band_omega_[b] = sumw/count;
+            band_phi_[b] = sumphi/count;
+        }
     }
 
     void checkSteepness() const {
@@ -324,61 +373,9 @@ private:
     }
 
     inline size_t upper_index(int i,int j) const {
-        const size_t N = (size_t)N_FREQ;
-        size_t ii = (size_t)i;
+        const size_t N=(size_t)N_FREQ;
+        size_t ii=(size_t)i;
         return ii*N-(ii*(ii-1))/2 + (size_t)(j-i);
-    }
-
-    // --- Core cached evaluation ---
-    template<typename FuncFirst, typename FuncSecond>
-    WaveState evalLagrangianCached(double x, double y, double t,
-                                   FuncFirst computeFirst,
-                                   FuncSecond computeSecond) const
-    {
-        Eigen::Vector3d disp=Eigen::Vector3d::Zero();
-        Eigen::Vector3d vel=Eigen::Vector3d::Zero();
-        Eigen::Vector3d acc=Eigen::Vector3d::Zero();
-
-        Eigen::ArrayXd theta0 = kx_.array()*x + ky_.array()*y + phi_.array();
-        static std::vector<double> theta2_static;
-        if(theta2_static.size() != pairwise_size_){
-            theta2_static.resize(pairwise_size_);
-            for(int i=0;i<N_FREQ;++i)
-                for(int j=i;j<N_FREQ;++j)
-                    theta2_static[upper_index(i,j)] = kx_sum_flat_[upper_index(i,j)]*x +
-                                                     ky_sum_flat_[upper_index(i,j)]*y +
-                                                     phi_sum_flat_[upper_index(i,j)];
-        }
-
-        if(trig_cache_.last_t != t){
-            trig_cache_.sin_first = (theta0 - omega_.array()*t).sin();
-            trig_cache_.cos_first = (theta0 - omega_.array()*t).cos();
-
-            trig_cache_.sin_second.resize(pairwise_size_);
-            trig_cache_.cos_second.resize(pairwise_size_);
-            for(size_t idx=0; idx<pairwise_size_; ++idx){
-                double th = theta2_static[idx] - omega_sum_flat_[idx]*t;
-                trig_cache_.sin_second(idx) = std::sin(th);
-                trig_cache_.cos_second(idx) = std::cos(th);
-            }
-            trig_cache_.last_t = t;
-        }
-
-        for(int i=0;i<N_FREQ;++i)
-            computeFirst(disp, vel, acc, i, trig_cache_.sin_first(i), trig_cache_.cos_first(i));
-
-        for(int i=0;i<N_FREQ;++i)
-            for(int j=i;j<N_FREQ;++j){
-                size_t idx=upper_index(i,j);
-                double factor=(i==j)?1.0:2.0;
-                computeSecond(disp, vel, acc, idx, factor,
-                              trig_cache_.sin_second(idx), trig_cache_.cos_second(idx));
-            }
-
-        vel.x() += stokes_drift_mean_xy_.x();
-        vel.y() += stokes_drift_mean_xy_.y();
-
-        return {disp, vel, acc};
     }
 };
 
