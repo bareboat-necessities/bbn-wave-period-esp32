8e01848d3c121675018aa279bc28542469b1ea35
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-08-29 23:47:32 -0400

    Update WaveSpectrumEstimator.h

--- bbn_wave_freq_m5atomS3/WaveSpectrumEstimator.h
+++ bbn_wave_freq_m5atomS3/WaveSpectrumEstimator.h
@@ -39,6 +39,8 @@
 template<int Nfreq = 32, int Nblock = 1024>
 class WaveSpectrumEstimator {
 public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
     static constexpr double g = 9.80665;
     using Vec = Eigen::Matrix<double, Nfreq, 1>;
 
