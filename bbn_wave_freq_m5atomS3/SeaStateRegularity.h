efd9b6daa9c5fd1acff8d4e0fb41e58ed966af54
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-08-29 23:41:26 -0400

    Update SeaStateRegularity.h

--- bbn_wave_freq_m5atomS3/SeaStateRegularity.h
+++ bbn_wave_freq_m5atomS3/SeaStateRegularity.h
@@ -26,6 +26,8 @@
  */
 class SeaStateRegularity {
 public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
     // Configurable constants
     constexpr static float EPSILON = 1e-12f;
     constexpr static float HEIGHT_R_HI = 0.98f;
