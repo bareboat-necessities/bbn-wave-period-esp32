c7367a53fb4269879cf3d9394a1101adf582e43b
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-08-31 15:21:29 -0400

    Update KalmanWaveDirection.h

--- bbn_wave_freq_m5atomS3/KalmanWaveDirection.h
+++ bbn_wave_freq_m5atomS3/KalmanWaveDirection.h
@@ -229,7 +229,6 @@ void KalmanWaveDirection_test_1() {
   generator.seed(239);
   std::normal_distribution<float> dist(mean, stddev);
 
-  alignas(EIGEN_MAX_ALIGN_BYTES)
   KalmanWaveDirection filter(freq, delta_t);
   filter.setMeasurementNoise(0.01f);
   filter.setProcessNoise(1e-6f);
