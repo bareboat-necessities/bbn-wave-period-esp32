8a4db1206c9e3004465a6eb1b616b3398af0a71c
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-08-31 12:51:26 -0400

    Update PiersonMoskowitzStokes3D_Waves.h

--- bbn_wave_freq_m5atomS3/PiersonMoskowitzStokes3D_Waves.h
+++ bbn_wave_freq_m5atomS3/PiersonMoskowitzStokes3D_Waves.h
@@ -27,7 +27,7 @@
 #endif
 
 template<int N_FREQ = 256>
-class EIGEN_ALIGN_MAX PiersonMoskowitzSpectrum {
+class PiersonMoskowitzSpectrum {
 public:
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
@@ -112,7 +112,7 @@ private:
 
 // PMStokesN3dWaves
 template<int N_FREQ = 256, int ORDER = 5>
-class EIGEN_ALIGN_MAX PMStokesN3dWaves {
+class PMStokesN3dWaves {
     static_assert(ORDER >= 1 && ORDER <= 5, "ORDER supported range is 1..5");
 
 public:
@@ -269,7 +269,7 @@ private:
 void generateWavePMStokesCSV(const std::string& filename,
                              double Hs, double Tp, double mean_dir_deg,
                              double duration = 40.0, double dt = 0.005) {
-    alignas(EIGEN_MAX_ALIGN_BYTES)
+    
     PMStokesN3dWaves<256, 5> waveModel(
         Hs, Tp, mean_dir_deg, 0.02, 0.8, 9.81, 15.0, 239u
     );
