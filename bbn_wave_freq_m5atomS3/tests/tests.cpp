93fab82cbeeb3c357416f153708f108e703c8627
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-09-02 18:14:20 -0400

    Update tests.cpp

--- bbn_wave_freq_m5atomS3/tests/tests.cpp
+++ bbn_wave_freq_m5atomS3/tests/tests.cpp
@@ -213,7 +213,8 @@ int main(int argc, char *argv[]) {
       t += delta_t;
     }
   } else if (test_type == TestType::JONSWAP) {
-    auto waveModel = std::make_unique<Jonswap3dStokesWaves<128>>(w->amplitude(), w->period(), 30.0 /*dir*/, 0.02, 0.8, 2.0, g_std, 15.0);
+    auto dist = std::make_shared<Cosine2sRandomizedDistribution>(mean_dir_deg * PI / 180.0, 10.0, 42u);
+    auto waveModel = std::make_unique<Jonswap3dStokesWaves<128>>(w->amplitude(), w->period(), dist, 0.02, 0.8, 2.0, g_std);
     while (t < test_duration) {
        auto state = waveModel->getSurfaceState(0.0, 0.0, t);
        float zero_mean_gauss_noise = dist(generator);
