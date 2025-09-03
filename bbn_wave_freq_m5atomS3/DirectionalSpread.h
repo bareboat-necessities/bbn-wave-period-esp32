dcf597113802666670aa93b019068924bef533fa
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-09-03 12:17:10 -0400

    Update DirectionalSpread.h

--- bbn_wave_freq_m5atomS3/DirectionalSpread.h
+++ bbn_wave_freq_m5atomS3/DirectionalSpread.h
@@ -169,9 +169,12 @@ public:
 
     double operator()(double theta, double f) const override {
         double s_f = s0_ * std::pow(f / f0_, m_);
-        double norm = std::exp(std::lgamma(s_f + 0.5) - std::lgamma(s_f + 1.0) - 0.5 * std::log(PI));
+        double norm = std::exp(std::lgamma(s_f + 0.5)
+                             - std::lgamma(s_f + 1.0)
+                             - 0.5 * std::log(PI));
         double dtheta = theta - mean_dir_rad_;
-        return norm * std::pow(std::max(0.0, std::cos(0.5 * dtheta)), 2.0 * s_f);
+        return norm * std::pow(std::max(0.0, std::cos(0.5 * dtheta)),
+                               2.0 * s_f);
     }
 
     std::vector<double> weights(int M, double f) const override {
