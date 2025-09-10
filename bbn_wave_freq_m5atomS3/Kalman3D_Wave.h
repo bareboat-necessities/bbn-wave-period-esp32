6c91a7345820a9dca99d2b565a2f106e68b8e9cd
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-09-09 21:31:58 -0400

    Update Kalman3D_Wave.h

--- bbn_wave_freq_m5atomS3/Kalman3D_Wave.h
+++ bbn_wave_freq_m5atomS3/Kalman3D_Wave.h
@@ -443,9 +443,12 @@ void Kalman3D_Wave<T, with_bias>::measurement_update_mag_only(Vector3 const& mag
     measurement_update_partial(mag, v2hat, Rmag);
 }
 
+// specific force prediction: f_b = R^T (a_w - g)
 template<typename T, bool with_bias>
-Matrix<T, 3, 1> Kalman3D_Wave<T, with_bias>::accelerometer_measurement_func() const {
-    return qref.inverse() * v1ref;
+Matrix<T,3,1> Kalman3D_Wave<T, with_bias>::accelerometer_measurement_func() const {
+    const Vector3 g_world(0,0,gravity_magnitude);
+    const Vector3 aw = xext.template segment<3>(OFF_AW);
+    return Rt_from_quat() * (aw - g_world);
 }
 
 template<typename T, bool with_bias>
