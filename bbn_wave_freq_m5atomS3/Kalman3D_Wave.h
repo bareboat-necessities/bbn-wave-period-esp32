5ec975a782a647f87968de6c47316b5ed3fb147d
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-09-11 17:48:13 -0400

    Update Kalman3D_Wave.h

--- bbn_wave_freq_m5atomS3/Kalman3D_Wave.h
+++ bbn_wave_freq_m5atomS3/Kalman3D_Wave.h
@@ -314,8 +314,8 @@ Kalman3D_Wave<T, with_bias>::quaternion_from_acc(Vector3 const& acc)
     Vector3 an = acc.normalized();
     Vector3 zb = Vector3::UnitZ();
 
-    // Rotate zb to (-an)
-    Vector3 target = -an;
+    // Rotate zb to an
+    Vector3 target = an;
     T cos_theta = zb.dot(target);
     Vector3 axis = zb.cross(target);
     T norm_axis = axis.norm();
