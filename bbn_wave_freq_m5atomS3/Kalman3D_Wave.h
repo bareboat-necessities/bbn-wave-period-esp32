8e42b368c2de7c7286b201ff13eb93bd4586349e
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-09-12 19:37:32 -0400

    Update Kalman3D_Wave.h

--- bbn_wave_freq_m5atomS3/Kalman3D_Wave.h
+++ bbn_wave_freq_m5atomS3/Kalman3D_Wave.h
@@ -171,7 +171,7 @@ class EIGEN_ALIGN_MAX Kalman3D_Wave {
     MatrixNX Qext; // Extended process noise / Q
 
     // World-acceleration OU process a_w dynamics parameters
-    T tau_aw = T(1.5);            // correlation time [s], tune 1–5 s for sea states
+    T tau_aw = T(1.0);            // correlation time [s], tune 1–5 s for sea states
     Matrix3 Sigma_aw_stat = Matrix3::Identity() * T(0.5*0.5); // stationary variance diag [ (m/s^2)^2 ]
 
     // convenience getters
