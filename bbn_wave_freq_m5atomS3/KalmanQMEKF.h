891eeaad197e3c1f3a49fca23c47c61f2f5450d9
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-09-14 19:15:45 -0400

    Update KalmanQMEKF.h

--- bbn_wave_freq_m5atomS3/KalmanQMEKF.h
+++ bbn_wave_freq_m5atomS3/KalmanQMEKF.h
@@ -195,7 +195,7 @@ void QuaternionMEKF<T, with_bias>::time_update(Vector3 const& gyr, T Ts) {
   }
 
   // Quaternionf.coeffs() get the components in [x,y,z,w] order
-  qref = F * qref.coeffs();
+  qref.coeffs() = F * qref.coeffs();
   qref.normalize();
 
   MatrixN F_a;
