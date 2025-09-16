35fc16724c0ac42f88cad58260a0d063f60ecaa7
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-09-16 10:44:42 -0400

    Update Kalman3D_Wave.h

--- bbn_wave_freq_m5atomS3/Kalman3D_Wave.h
+++ bbn_wave_freq_m5atomS3/Kalman3D_Wave.h
@@ -74,7 +74,7 @@ class EIGEN_ALIGN_MAX Kalman3D_Wave {
 
     // Constructor signatures preserved, additional defaults for linear process noise
     Kalman3D_Wave(Vector3 const& sigma_a, Vector3 const& sigma_g, Vector3 const& sigma_m,
-                  T Pq0 = T(1e-6), T Pb0 = T(1e-1), T b0 = T(1e-12), T R_S_noise = T(2e-1),
+                  T Pq0 = T(1e-6), T Pb0 = T(1e-1), T b0 = T(1e-12), T R_S_noise = T(2e-2),
                   T gravity_magnitude = T(9.80665));
 
     // Initialization / measurement API 
