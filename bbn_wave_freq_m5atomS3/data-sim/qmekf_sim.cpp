7d427b100134f3678388492e21060b24edf2382d
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-09-16 16:36:47 -0400

    Update qmekf_sim.cpp

--- bbn_wave_freq_m5atomS3/data-sim/qmekf_sim.cpp
+++ bbn_wave_freq_m5atomS3/data-sim/qmekf_sim.cpp
@@ -12,7 +12,7 @@
 #endif
 
 const float g_std = 9.80665f;      // standard gravity acceleration m/s²
-const double MAG_DELAY_SEC = 5.0; // delay before using magnetometer
+const float MAG_DELAY_SEC = 5.0f; // delay before using magnetometer
 
 #include "KalmanQMEKF.h"       // Q-MEKF filter (patched with set_mag_world_ref)
 #include "WaveFilesSupport.h"  // file reader/parser + naming
