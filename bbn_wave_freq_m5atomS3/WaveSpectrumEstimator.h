6b60cd1fbba7adbd621b9d9e6eaa63de1e1b416b
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-09-30 19:03:59 -0400

    Update WaveSpectrumEstimator.h

--- bbn_wave_freq_m5atomS3/WaveSpectrumEstimator.h
+++ bbn_wave_freq_m5atomS3/WaveSpectrumEstimator.h
@@ -84,7 +84,7 @@ public:
     };
 
     WaveSpectrumEstimator(double fs_raw_ = 240.0,
-                          int decimFactor_ = 75,
+                          int decimFactor_ = 80,
                           bool hannEnabled_ = true)
         : fs_raw(fs_raw_), decimFactor(decimFactor_), hannEnabled(hannEnabled_)
     {
