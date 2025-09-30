115f6cd1ea85888422d514c29e6813b8e9b0e5ef
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-09-30 19:16:29 -0400

    Update WaveSpectrumEstimator.h

--- bbn_wave_freq_m5atomS3/WaveSpectrumEstimator.h
+++ bbn_wave_freq_m5atomS3/WaveSpectrumEstimator.h
@@ -561,8 +561,8 @@ private:
 
     // ---------- Smoothing controls (FIX) ----------
     bool  use_psd_ema = true;
-    double ema_alpha_low  = 0.25;  // alpha near lowest f
-    double ema_alpha_high = 0.07;  // alpha near highest f (smaller -> stronger smoothing)
+    double ema_alpha_low  = 0.20;  // alpha near lowest f
+    double ema_alpha_high = 0.06;  // alpha near highest f (smaller -> stronger smoothing)
     bool  have_ema = false;
     Eigen::Matrix<double, Nfreq, 1> psd_ema_;
 
