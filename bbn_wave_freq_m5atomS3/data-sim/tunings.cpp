77947cdb28351bc315d3143dff5c61dde7a24cc5
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-10-06 12:33:24 -0400

    Update tunings.cpp

--- bbn_wave_freq_m5atomS3/data-sim/tunings.cpp
+++ bbn_wave_freq_m5atomS3/data-sim/tunings.cpp
@@ -17,7 +17,7 @@ static constexpr double TWO_PI = 2.0 * M_PI;
 static constexpr double g_std  = 9.80665;
 
 // R_S law
-inline float R_S_law(float Tp, float R_S_base = 1.13f, float T_p_base = 8.5f) {
+inline float R_S_law(float Tp, float R_S_base = 1.9f, float T_p_base = 8.5f) {
     return R_S_base * std::pow(Tp/T_p_base, 1.0 / 3.0);
 }
 
