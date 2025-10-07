d6b9ff2a833c37e0c881f2a5b203b3ac50ca7c14
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-10-07 19:18:03 -0400

    Update SeaStateRegularity.h

--- bbn_wave_freq_m5atomS3/SeaStateRegularity.h
+++ bbn_wave_freq_m5atomS3/SeaStateRegularity.h
@@ -228,8 +228,7 @@ const float Rm = (g * g) * R_demod_eff;
         // Posterior displacement PSD (ENBW-compensated)
         const float mu2  = mu_r_[i] * mu_r_[i] + mu_i_[i] * mu_i_[i];
         const float trP  = P_rr_[i] + P_ii_[i];
-        // Posterior displacement PSD (ENBW-compensated, ω⁴-corrected)
-        const float Seta = 0.5f * (mu2 + trP) / (enbw_rad_[i]);
+        const float Seta = 0.5f * (mu2) / (enbw_rad_[i]);
        
         // Peak metric on ω·Sη prevents low-ω bias
         Epow_pk_[i]   = w_[i] * Seta;
