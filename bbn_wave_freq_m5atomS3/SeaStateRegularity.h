2afeb87e206e17a84e0104264cdcbfd479c0ef19
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-10-09 22:26:39 -0400

    Update SeaStateRegularity.h

--- bbn_wave_freq_m5atomS3/SeaStateRegularity.h
+++ bbn_wave_freq_m5atomS3/SeaStateRegularity.h
@@ -228,7 +228,7 @@ public:
       // derive exponential weight α from time constant
       const float alpha = 1.0f - std::exp(-dt_s / tau_spec);
 
-      for (int i = 0; i < NBINS; ++i) {
+      for (int i = 0; i < SeaStateRegularity<NK>::NBINS; ++i) {
         float f_i = S.omega[i] / (2.0f * float(M_PI));  // Hz
         float Srad = S.S_eta_rad[i];                    // m²/(rad/s)
         if (!(Srad > 0.0f)) continue;
