c533377ed6a34b8af7011ce16a353bbd832d00c9
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-10-13 20:48:46 -0400

    Update Kalman3D_Wave.h

--- bbn_wave_freq_m5atomS3/Kalman3D_Wave.h
+++ bbn_wave_freq_m5atomS3/Kalman3D_Wave.h
@@ -1107,14 +1107,14 @@ void Kalman3D_Wave<T, with_gyro_bias, with_accel_bias>::QdAxis4x1_analytic(
     T tau, T h, T sigma2, Eigen::Matrix<T,4,4>& Qd_axis)
 {
     // Dimensionless step and OU intensity (all float)
-    const T inv_tau = T(1) / std::max(tau, T(1e-8f));
+    const T inv_tau = T(1) / std::max(tau, T(1e-7f));
     const T x = h * inv_tau;
     const T q_c = (T(2) * sigma2) * inv_tau;
 
     Eigen::Matrix<T,4,4> K; K.setZero();
 
-    if (x < T(1e-3f)) {
-        // -------- Small-x Maclaurin (float) --------
+    if (x < T(0.05f)) {
+        // Small-x Maclaurin (float)
         const T h2 = h*h, h3 = h2*h, h4 = h3*h, h5 = h4*h, h6 = h5*h, h7 = h6*h, h8 = h7*h;
 
         // Variances
@@ -1138,7 +1138,7 @@ void Kalman3D_Wave<T, with_gyro_bias, with_accel_bias>::QdAxis4x1_analytic(
         K(2,0)=K_Sv; K(2,1)=K_Sp; K(2,2)=K_SS; K(2,3)=K_Sa;
         K(3,0)=K_va; K(3,1)=K_pa; K(3,2)=K_Sa; K(3,3)=K_aa;
     } else {
-        // -------- General-x (float) with FMA everywhere --------
+        // General-x (float) with FMA everywhere
         const auto P = make_prims<T>(h, tau);   // {x, alpha, em1, alpha2, em1_2}
         const T tau2 = tau*tau;
         const T tau3 = tau2*tau;
