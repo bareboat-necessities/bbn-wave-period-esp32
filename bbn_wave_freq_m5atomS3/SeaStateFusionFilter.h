865a9f6c9573c8af181a34217511007cb6a3acc6
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-10-24 17:40:35 -0400

    Update SeaStateFusionFilter.h

--- bbn_wave_freq_m5atomS3/SeaStateFusionFilter.h
+++ bbn_wave_freq_m5atomS3/SeaStateFusionFilter.h
@@ -68,8 +68,8 @@ constexpr float MAX_R_S     = 20.0f;
 
 constexpr float R_S_coeff   = 0.6f;
 
-constexpr float ADAPT_TAU_SEC = 3.0f;
-constexpr float ADAPT_EVERY_SECS = 0.05f;
+constexpr float ADAPT_TAU_SEC = 10.0f;
+constexpr float ADAPT_EVERY_SECS = 0.1f;
 constexpr float ONLINE_TUNE_WARMUP_SEC = 40.0f;
 constexpr float MAG_DELAY_SEC = 5.0f;
 
