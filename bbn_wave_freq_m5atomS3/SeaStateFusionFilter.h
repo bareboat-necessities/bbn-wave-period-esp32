d1b56ed6c2c5765605b5d00543d5686e6ce61b3f
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-11-03 17:18:15 -0500

    Update SeaStateFusionFilter.h

--- bbn_wave_freq_m5atomS3/SeaStateFusionFilter.h
+++ bbn_wave_freq_m5atomS3/SeaStateFusionFilter.h
@@ -158,6 +158,7 @@ public:
     {
         mekf_ = std::make_unique<Kalman3D_Wave<float,true,true>>(sigma_a, sigma_g, sigma_m);
         apply_tune();
+        mekf_->set_aw_stationary_corr_std(sigma_a);
     }
 
     void initialize_from_acc(const Eigen::Vector3f& acc_world) {
