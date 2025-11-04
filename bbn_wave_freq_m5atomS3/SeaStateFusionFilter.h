b163c7a25fc292aa066148c65c48bf3301695cb1
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-11-03 20:06:01 -0500

    Update SeaStateFusionFilter.h

--- bbn_wave_freq_m5atomS3/SeaStateFusionFilter.h
+++ bbn_wave_freq_m5atomS3/SeaStateFusionFilter.h
@@ -275,7 +275,7 @@ private:
     bool freq_init_ = false;
 
     static constexpr float R_S_xy_factor = 0.05f;
-    static constexpr float S_factor = 4.0f;
+    static constexpr float S_factor = 1.5f;
 
     TrackingPolicy tracker_policy_{};  // one instance of frequency tracker per filter
     FrequencySmoother<float> freqSmoother;
