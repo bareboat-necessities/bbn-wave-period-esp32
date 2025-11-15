2093855357918d52713a40eed4f03f6e04e84030
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-11-14 22:32:10 -0500

    Update w3d_sim_adapt_3.cpp

--- bbn_wave_freq_m5atomS3/data-sim/w3d_sim_adapt_3.cpp
+++ bbn_wave_freq_m5atomS3/data-sim/w3d_sim_adapt_3.cpp
@@ -33,7 +33,7 @@ const float FAIL_ERR_LIMIT_PERCENT_X_LOW  = 33.0f;
 const float FAIL_ERR_LIMIT_PERCENT_Y_LOW  = 33.0f;
 const float FAIL_ERR_LIMIT_PERCENT_Z_LOW  = 13.0f;
 
-const float FAIL_ERR_LIMIT_YAW_DEG = 3.0f;  
+const float FAIL_ERR_LIMIT_YAW_DEG = 4.0f;  
 
 constexpr float RMS_WINDOW_SEC = 60.0f;  // RMS window
 
