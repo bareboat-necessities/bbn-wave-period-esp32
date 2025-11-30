ddc112e6b451c6e5838623e4b7a9c0fd6d5f0ccc
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-11-29 21:11:31 -0500

    Update SeaStateFusionFilter.h

--- bbn_wave_freq_m5atomS3/SeaStateFusionFilter.h
+++ bbn_wave_freq_m5atomS3/SeaStateFusionFilter.h
@@ -104,7 +104,7 @@ struct TrackerPolicy<TrackerType::ARANOVSKIY> {
     }
 
     double run(float a, float dt) {
-        t.update((double)a, (double)dt);
+        t.update((double)a / g_std, (double)dt);
         return t.getFrequencyHz();
     }
 };
@@ -116,7 +116,7 @@ struct TrackerPolicy<TrackerType::KALMANF> {
     Tracker t = Tracker();
     double run(float a, float dt) {
         double e;
-        double freq = t.process((double)a, (double)dt, &e);
+        double freq = t.process((double)a / g_std, (double)dt, &e);
         return freq;
     }
 };
