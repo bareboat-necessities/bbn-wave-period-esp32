5cf08a8a76b5569ce3cc1f878f62f471868f6516
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-12-02 19:20:16 -0500

    Update SeaStateFusionFilter.h

--- bbn_wave_freq_m5atomS3/SeaStateFusionFilter.h
+++ bbn_wave_freq_m5atomS3/SeaStateFusionFilter.h
@@ -543,8 +543,8 @@ private:
     float sigma_target_ = NAN;
     float RS_target_    = NAN;
 
-    float R_S_coeff_    = 1.5f;
-    float tau_coeff_    = 1.4f;
+    float R_S_coeff_    = 0.9f;
+    float tau_coeff_    = 1.1f;
 
     std::unique_ptr<Kalman3D_Wave<float,true,true>>  mekf_;
     KalmanWaveDirection                              dir_filter_{2.0f * static_cast<float>(M_PI) * FREQ_GUESS};
