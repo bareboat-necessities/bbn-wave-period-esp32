7b50ee736a8c6ed05f7b9abaef247913eea1f804
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-12-09 18:41:40 -0500

    Update w3d_sim_adapt_3.cpp

--- bbn_wave_freq_m5atomS3/data-sim/w3d_sim_adapt_3.cpp
+++ bbn_wave_freq_m5atomS3/data-sim/w3d_sim_adapt_3.cpp
@@ -344,7 +344,7 @@ static void process_wave_file_for_tracker(const std::string &filename,
 //  Main
 int main(int argc, char* argv[]) {
     float dt = 1.0f / 240.0f;
-    bool with_mag = false;
+    bool with_mag = true;
     add_noise = true;
 
     for (int i = 1; i < argc; i++) {
