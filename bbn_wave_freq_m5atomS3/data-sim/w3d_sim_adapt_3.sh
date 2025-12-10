4092b72df0eda3b4d91bbf528d5d9b8b8c0f3331
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-12-09 22:04:20 -0500

    Update w3d_sim_adapt_3.sh

--- bbn_wave_freq_m5atomS3/data-sim/w3d_sim_adapt_3.sh
+++ bbn_wave_freq_m5atomS3/data-sim/w3d_sim_adapt_3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-./w3d_sim_adapt_3 --nomag
+#./w3d_sim_adapt_3 --nomag       # it seems not possible to make filter stable without using magnetomenter
 ./w3d_sim_adapt_3
 ./imu_wave_dir
