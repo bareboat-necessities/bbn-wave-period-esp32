a72819566495041e0b4725f04ba00adbb94b8fd2
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-12-12 18:46:53 -0500

    Update Kalman3D_Wave.h

--- bbn_wave_freq_m5atomS3/Kalman3D_Wave.h
+++ bbn_wave_freq_m5atomS3/Kalman3D_Wave.h
@@ -4,7 +4,7 @@
     TODO:
 
     R_S in contructor treated as var instead of std?
-    Level arm correction logic wrong in many places.
+    Level arm correction logic wrong in many places. Lever-arm model: Jacobians are incomplete if you ever want to estimate gyro bias tightly
     PSD projection for N=12 (find better pattern)
     linear_block_enabled_ == false does NOT actually freeze the linear block
     
