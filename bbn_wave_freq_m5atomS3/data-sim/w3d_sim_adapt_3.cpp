6c28371a59b6fbb1b4633cced6558224b6831aeb
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-12-12 23:06:04 -0500

    Update w3d_sim_adapt_3.cpp

--- bbn_wave_freq_m5atomS3/data-sim/w3d_sim_adapt_3.cpp
+++ bbn_wave_freq_m5atomS3/data-sim/w3d_sim_adapt_3.cpp
@@ -225,7 +225,7 @@ static void process_wave_file_for_tracker(const std::string &filename,
 
     const Vector3f sigma_a_init(0.25f, 0.25f, 0.25f);
     const Vector3f sigma_g(0.00234f, 0.00234f, 0.00234f);
-    const Vector3f sigma_m(0.15f, 0.15f, 0.15f);
+    const Vector3f sigma_m(3.15f, 3.15f, 3.15f);
     filter.initialize(sigma_a_init, sigma_g, sigma_m);
 
     // Magnetic reference (same each run)
