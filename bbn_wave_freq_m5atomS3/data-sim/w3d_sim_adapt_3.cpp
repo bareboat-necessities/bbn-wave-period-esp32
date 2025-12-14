608228c6f7e38f3229c20599a73330306402603a
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-12-14 10:43:43 -0500

    Update w3d_sim_adapt_3.cpp

--- bbn_wave_freq_m5atomS3/data-sim/w3d_sim_adapt_3.cpp
+++ bbn_wave_freq_m5atomS3/data-sim/w3d_sim_adapt_3.cpp
@@ -306,7 +306,7 @@ Vector3f mag_body_ned_hold = Vector3f::Zero();
     // Filter
     const Vector3f sigma_a_init(2.0f * acc_sigma, 2.0f * acc_sigma, 2.0f * acc_sigma);
     const Vector3f sigma_g(2.0f * gyr_sigma, 2.0f * gyr_sigma, 2.0f * gyr_sigma);    
-    const float sigma_m_uT = 1.2f * mag_sigma_uT;   // a bit conservative
+    const float sigma_m_uT = 2.0f * mag_sigma_uT;   // a bit conservative
     const Vector3f sigma_m(sigma_m_uT, sigma_m_uT, sigma_m_uT);
     filter.initialize(sigma_a_init, sigma_g, sigma_m);
     
