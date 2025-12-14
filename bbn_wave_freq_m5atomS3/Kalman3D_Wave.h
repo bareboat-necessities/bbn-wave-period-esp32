b2d87be86d44799f5d5c97ae4e70af416f480193
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-12-13 23:59:46 -0500

    Update Kalman3D_Wave.h

--- bbn_wave_freq_m5atomS3/Kalman3D_Wave.h
+++ bbn_wave_freq_m5atomS3/Kalman3D_Wave.h
@@ -543,7 +543,7 @@ class Kalman3D_Wave {
     // Last gyro
     Vector3 last_gyr_bias_corrected{};
 
-    T sigma_bacc0_ = T(0.05); // initial accel bias std
+    T sigma_bacc0_ = T(0.005); // initial accel bias std
     Matrix3 Q_bacc_ = Matrix3::Identity() * T(1e-8);
 
     // Accelerometer bias temperature coefficient (per-axis), units: m/s^2 per °C.
@@ -551,7 +551,7 @@ class Kalman3D_Wave {
     Vector3 k_a_ = Vector3::Constant(T(0.003));
 
     T sigma_bmag0_ = T(8.0);                 // µT 
-    Matrix3 Q_bmag_ = Matrix3::Identity() * T(1e-8); // (µT^2)/s  (tune)
+    Matrix3 Q_bmag_ = Matrix3::Identity() * T(1e-7); // (µT^2)/s  (tune)
               
     // Constant matrices
     Matrix3 Rmag;
