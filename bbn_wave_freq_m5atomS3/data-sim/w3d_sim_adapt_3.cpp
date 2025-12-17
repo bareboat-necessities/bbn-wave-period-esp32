0df0db19d645fe00c25ac846ec03ec5968e3cb9d
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-12-17 15:25:57 -0500

    Update w3d_sim_adapt_3.cpp

--- bbn_wave_freq_m5atomS3/data-sim/w3d_sim_adapt_3.cpp
+++ bbn_wave_freq_m5atomS3/data-sim/w3d_sim_adapt_3.cpp
@@ -320,7 +320,7 @@ static void process_wave_file_for_tracker(const std::string &filename, float dt,
     
     // mag ref policy
     cfg.mag_delay_sec = MAG_DELAY_SEC;
-    cfg.use_fixed_mag_world_ref = true;   // TODO: switch to false (it needs to learn it by itself)
+    cfg.use_fixed_mag_world_ref = false;   // TODO: switch to false (it needs to learn it by itself)
     cfg.mag_world_ref = mag_world_a;
     
     // warmup policy 
