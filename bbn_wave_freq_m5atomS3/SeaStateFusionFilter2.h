c6ed6fe303d29ec0e3e0ab6edb3810eeee85d82e
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-02-24 19:00:23 -0500

    Update SeaStateFusionFilter2.h

--- bbn_wave_freq_m5atomS3/SeaStateFusionFilter2.h
+++ bbn_wave_freq_m5atomS3/SeaStateFusionFilter2.h
@@ -854,6 +854,9 @@ private:
       warmup_Racc_active_ = false;
     }
 
+    // Start spectral estimation fresh in Live (avoid warmup/transition contamination)
+    reset_spectrum_adapter_();
+    
     apply_oscillators_tune_();
     apply_adaptive_rms_tuning_();
     last_adapt_time_sec_ = time_;
