ff74dcc08e0614aa7f0b6a683992834a69540430
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-03-05 13:06:23 -0500

    Update ImuCalWizardRunner.h

--- bbn_wave_freq_m5atomS3/ImuCalWizardRunner.h
+++ bbn_wave_freq_m5atomS3/ImuCalWizardRunner.h
@@ -3,8 +3,18 @@
 #include "AtomS3R_ImuCal.h"
 #include "AtomS3R_M5Ui.h"
 
+#if !defined(NO_BOSCH_API)
+  #include "BoschBmi270_ImuCal.h"
+#endif
+
 namespace atoms3r_ical {
 
+// Bosch path (default build): caller must provide an initialized BoschBmi270_ImuCal source.
+#if !defined(NO_BOSCH_API)
+bool runImuCalWizard(M5Ui& ui, ImuCalStoreNvs& store, BoschBmi270_ImuCal& imu, ImuCalBlobV1& out_saved);
+#endif
+
+// Legacy path (only when NO_BOSCH_API is defined)
 bool runImuCalWizard(M5Ui& ui, ImuCalStoreNvs& store, ImuCalBlobV1& out_saved);
 
 }  // namespace atoms3r_ical
