788b9a7390e523d06a52a20945f96db607f280bf
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-03-05 23:26:36 -0500

    Update ImuCalWizardRunner.cpp

--- bbn_wave_freq_m5atomS3/ImuCalWizardRunner.cpp
+++ bbn_wave_freq_m5atomS3/ImuCalWizardRunner.cpp
@@ -1,26 +1,12 @@
-#include "ImuCalWizardRunner.h"
-#include "AtomS3R_ImuCalWizard.h"
+#pragma once
 
-#if ATOMS3R_HAVE_BOSCH_API
-  #include "BoschBmi270_ImuCal.h"
-#endif
+#include "AtomS3R_ImuCal.h"
+#include "AtomS3R_M5Ui.h"
 
 namespace atoms3r_ical {
 
-bool runImuCalWizard(M5Ui& ui, ImuCalStoreNvs& store, ImuCalBlobV1& out_saved) {
-  ImuCalWizard wizard(ui, store);
-  return wizard.runAndSave(out_saved);
-}
-
-#if ATOMS3R_HAVE_BOSCH_API
 bool runImuCalWizard(M5Ui& ui,
                      ImuCalStoreNvs& store,
-                     BoschBmi270_ImuCal& imu,
-                     ImuCalBlobV1& out_saved)
-{
-  ImuCalWizard wizard(ui, store, imu);
-  return wizard.runAndSave(out_saved);
-}
-#endif
+                     ImuCalBlobV1& out_saved);
 
 } // namespace atoms3r_ical
