091dd1e2168ea1f0637f2deee22478469c23a472
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-03-17 14:58:04 -0400

    Update w3d_sim_adapt_4.cpp

--- bbn_wave_freq_m5atomS3/data-sim/w3d_sim_adapt_4.cpp
+++ bbn_wave_freq_m5atomS3/data-sim/w3d_sim_adapt_4.cpp
@@ -25,8 +25,8 @@
 
 const float g_std = 9.80665f;     // standard gravity acceleration m/s²
 
-const float FAIL_ERR_LIMIT_PERCENT_Z_JONSWAP   = 12.5f;
-const float FAIL_ERR_LIMIT_PERCENT_Z_PMSTOKES  = 12.0f;
+const float FAIL_ERR_LIMIT_PERCENT_Z_JONSWAP   = 12.0f;
+const float FAIL_ERR_LIMIT_PERCENT_Z_PMSTOKES  = 11.0f;
 
 const float FAIL_ERR_LIMIT_YAW_DEG = 4.0f;  
 
