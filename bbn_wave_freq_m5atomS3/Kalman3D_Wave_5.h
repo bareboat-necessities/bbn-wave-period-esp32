e556392aac8efe3c01a6b28c85d4bf0abd6f7ffc
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-03-19 17:04:11 -0400

    Update Kalman3D_Wave_5.h

--- bbn_wave_freq_m5atomS3/Kalman3D_Wave_5.h
+++ bbn_wave_freq_m5atomS3/Kalman3D_Wave_5.h
@@ -484,8 +484,8 @@ class Kalman3D_Wave_5 {
     Matrix3 R_v0;
 
     bool auto_zero_position_pseudo_enabled_ = true;
-    bool auto_zero_velocity_pseudo_enabled_ = false;
-    T    auto_zero_pseudo_period_sec_       = T(0.5);
+    bool auto_zero_velocity_pseudo_enabled_ = true;
+    T    auto_zero_pseudo_period_sec_       = T(0.015);
     T    auto_zero_pseudo_elapsed_sec_      = T(0);
 
     bool linear_block_enabled_ = true;
