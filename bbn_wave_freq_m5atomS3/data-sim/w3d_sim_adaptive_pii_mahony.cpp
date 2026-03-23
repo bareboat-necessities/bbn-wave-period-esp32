68ed2ad8d4666a4a4ed7e1186488cc6b9ee616dd
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-03-23 00:20:18 -0400

    Update w3d_sim_adaptive_pii_mahony.cpp

--- bbn_wave_freq_m5atomS3/data-sim/w3d_sim_adaptive_pii_mahony.cpp
+++ bbn_wave_freq_m5atomS3/data-sim/w3d_sim_adaptive_pii_mahony.cpp
@@ -25,7 +25,7 @@ bool add_noise = true;
 static constexpr W3dFailureLimits FAIL_LIMITS{
     .err_limit_percent_z_jonswap = 17.7f,
     .err_limit_percent_z_pmstokes = 16.0f,
-    .err_limit_yaw_deg = 22.5f,
+    .err_limit_yaw_deg = 19.0f,
 };
 
 class FusionAdapterAdaptivePIIMahony final : public IW3dFusionAdapter {
