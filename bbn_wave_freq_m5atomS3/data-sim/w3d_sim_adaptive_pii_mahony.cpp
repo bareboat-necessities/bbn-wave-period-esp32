e07c67bb0930a2c6812f41235280284dbd13c9d5
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-03-23 17:54:52 -0400

    Update w3d_sim_adaptive_pii_mahony.cpp

--- bbn_wave_freq_m5atomS3/data-sim/w3d_sim_adaptive_pii_mahony.cpp
+++ bbn_wave_freq_m5atomS3/data-sim/w3d_sim_adaptive_pii_mahony.cpp
@@ -31,7 +31,7 @@ static constexpr W3dFailureLimits FAIL_LIMITS{
 
 class FusionAdapterAdaptivePIIMahony final : public IW3dFusionAdapter {
 public:
-    using HeaveFilter = marine_obs::AdaptiveVerticalPIIMahony<float, true, TrackerType::KALMANF>;
+    using HeaveFilter = marine_obs::AdaptiveVerticalPIIMahony<float, true, TrackerType::ARANOVSKIY>;
 
     FusionAdapterAdaptivePIIMahony(bool with_mag,
                                    const Vector3f& sigma_a_init,
