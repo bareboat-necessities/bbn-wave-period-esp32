170f70803c1e5cf6581d31e39539748a0f5ddff6
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-03-24 13:44:25 -0400

    Update w3d_sim_adapt_3.sh

--- bbn_wave_freq_m5atomS3/data-sim/w3d_sim_adapt_3.sh
+++ bbn_wave_freq_m5atomS3/data-sim/w3d_sim_adapt_3.sh
@@ -2,7 +2,7 @@
 
 #./w3d_sim_adapt_3 --nomag       # it seems not possible to make filter stable without using magnetomenter
 
-./w3d_sim_adapt_5
+#./w3d_sim_adapt_5
 ./w3d_sim_adaptive_pii_mahony
 ./w3d_sim_adapt_4
 ./w3d_sim_adapt_3
